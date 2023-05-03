7e953c548ffbd4060d695f0a7449a7d3c69bb27e
Xteom <mateodelaroche@gmail.com>
2023-05-03 00:21:52 -0600

    first terraform commit

---
+++ infra/buckets.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "COBI_landing_zone_2023" {
+  bucket = "COBI-landing-zone-2023"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "COBI_clean_data_2023" {
+  bucket = "COBI-clean-data-2023"
+  acl    = "private"
+}
+
