d86a3a89da395307ac603abb0f47f9cb38be909e
Xteom <mateodelaroche@gmail.com>
2023-05-08 01:17:47 -0600

    some buckets

--- infra/main.tf
+++ infra/main.tf
@@ -6,3 +6,13 @@ module "buckets" {
   source = "./buckets"
 }
 
+resource "aws_s3_bucket" "COBI_landing_zone_2023" {
+  bucket = "COBI-landing-zone-2023"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "COBI_clean_data_2023" {
+  bucket = "COBI-clean-data-2023"
+  acl    = "private"
+}
+
