64d85db57ab6a3f3491cbcd7f4fedc8fcdb505bb
Xteom <mateodelaroche@gmail.com>
2023-05-24 18:53:19 -0600

    remove module error

--- infra/main.tf
+++ infra/main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-module "buckets" {
-  source = "./buckets"
-}
-
 resource "aws_s3_bucket" "COBI_landing_zone_2023" {
   bucket = "COBI-landing-zone-2023"
   acl    = "private"
