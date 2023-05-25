5c37b68b4062f2c420641a6865358381af2e7b05
jorcazas <jorcazas@gmail.com>
2023-05-25 00:25:32 -0600

    set date min and date max of data retrieving (each month))

--- etl/copernicus.py
+++ etl/copernicus.py
@@ -76,10 +76,14 @@ def create_request(service_id, product_id, date, motu, directory_to, name, user,
         A dictionary with the parameters needed to download the data from the Copernicus API.
 
     ''' 
+    str_date = date.strftime('%Y-%m-%d')
+    year = str_date[:4]
+    month = str_date[5:7]
+    last_month = f'{int(month) - 1:02}' 
     return {"service_id": service_id,
             "product_id": product_id,
-            "date_min": datetime.strptime('2017-01-01', '%Y-%m-%d').date(),
-            "date_max": datetime.strptime(date, '%Y-%m-%d').date(),
+            "date_min": datetime.strptime(f'{year}-{last_month}-01', '%Y-%m-%d').date(),
+            "date_max": date,
             "longitude_min": -116.,
             "longitude_max": -113.,
             "latitude_min": 26.,
@@ -110,14 +114,15 @@ def make_request(credentials,
 
     # Read the CSV file with the list of variables to download from the Copernicus API
     var_dict_list = read_variable_list(os.path.join(file_path, "infra", "copernicus_var_dict.csv"))
+    date = datetime.today()
 
     for dict in var_dict_list:
         try:
-            date = datetime.today().strftime('%Y-%m-%d')
+            
 
             # Construct a relative file path to the data directory
 
-            directory_to = os.path.join(destination_path, 'data', 'copernicus', 'raw')
+            directory_to = destination_path
             if not os.path.isdir(directory_to):
                 os.makedirs(directory_to, exist_ok=True)
 
