394690a53357f1441749e5408e9d64563198cde9
Xteom <mateodelaroche@gmail.com>
2023-05-25 19:08:10 -0600

    aws_s3_object updated & ecr repos commented

--- infra/main.tf
+++ infra/main.tf
@@ -35,43 +35,43 @@ resource "aws_s3_bucket" "COBI_lambda_layers_2023" {
 ###
 
 # upload Copernicus historical data to S3
-resource "aws_s3_bucket_object" "BioGeoChemicalOptics" {
+resource "aws_s3_object" "BioGeoChemicalOptics" {
   bucket = aws_s3_bucket.COBI_clean_data_2023.id
   key    = "BioGeoChemicalOptics.csv" #path to the csv file
   source = "./historical_copernicus/BioGeoChemicalOptics.csv" #path to the csv file
 }
 
-resource "aws_s3_bucket_object" "Plankton" {
+resource "aws_s3_object" "Plankton" {
   bucket = aws_s3_bucket.COBI_clean_data_2023.id
   key    = "Plankton.csv" #path to the csv file
   source = "./historical_copernicus/Plankton.csv" #path to the csv file
 }
 
-resource "aws_s3_bucket_object" "Reflectance" {
+resource "aws_s3_object" "Reflectance" {
   bucket = aws_s3_bucket.COBI_clean_data_2023.id
   key    = "Reflectance.csv" #path to the csv file
   source = "./historical_copernicus/Reflectance.csv" #path to the csv file
 }
 
-resource "aws_s3_bucket_object" "SeaSurfaceTemperature" {
+resource "aws_s3_object" "SeaSurfaceTemperature" {
   bucket = aws_s3_bucket.COBI_clean_data_2023.id
   key    = "SeaSurfaceTemperature.csv" #path to the csv file
   source = "./historical_copernicus/SeaSurfaceTemperature.csv" #path to the csv file
 }
 
-resource "aws_s3_bucket_object" "TotalSurfaceaAnd15mCurrent" {
+resource "aws_s3_object" "TotalSurfaceaAnd15mCurrent" {
   bucket = aws_s3_bucket.COBI_clean_data_2023.id
   key    = "TotalSurfaceaAnd15mCurrent.csv" #path to the csv file
   source = "./historical_copernicus/TotalSurfaceaAnd15mCurrent.csv" #path to the csv file
 }
 
-resource "aws_s3_bucket_object" "Transparence" {
+resource "aws_s3_object" "Transparence" {
   bucket = aws_s3_bucket.COBI_clean_data_2023.id
   key    = "Transparence.csv" #path to the csv file
   source = "./historical_copernicus/Transparence.csv" #path to the csv file
 }
 
-resource "aws_s3_bucket_object" "WaveHeight" {
+resource "aws_s3_object" "WaveHeight" {
   bucket = aws_s3_bucket.COBI_clean_data_2023.id
   key    = "WaveHeight.csv" #path to the csv file
   source = "./historical_copernicus/WaveHeight.csv" #path to the csv file
@@ -79,33 +79,58 @@ resource "aws_s3_bucket_object" "WaveHeight" {
 ###
 
 # upload Copernicus variable dictionary to S3
-resource "aws_s3_bucket_object" "copernicus_var_dict" {
+resource "aws_s3_object" "copernicus_var_dict" {
   bucket = aws_s3_bucket.COBI_input_data_2023.id
   key    = "copernicus_var_dict.csv" #path to the csv file
   source = "./input_data/copernicus_var_dict.csv" #path to the csv file
 }
 ###
 
+# # Create ECR repositories
+# this code was supposed to create ECR repositories for the Lambda functions and the upload the images to them
+# still figuring out how to do it, so for now I'm just uploading the images manually
+# resource "aws_ecr_repository" "copernicus_extract_lambda" {
+#   name = "copernicus_extract_lambda"
+# }
+
+# resource "aws_ecr_repository" "copernicus_transform_lambda" {
+#   name = "copernicus_transform_lambda"
+# }
+# ###
+
+# # upload Lambda code to ECR
+# resource "null_resource" "execute_script"{
+#   provisioner "local-exec" {
+#     command = <<EOF
+#       current_dir=$(pwd)
+#       cd ./lambdas/lambda_images/copernicus_extract_lambda && upload_ecr.sh
+#       cd ../copernicus_transform_lambda && upload_ecr.sh && upload_ecr.sh
+#       cd $current_dir
+#     EOF
+#   }
+# }
+# ###
+
 # upload Lambda layers to S3
-resource "aws_s3_bucket_object" "layer_requests" {
+resource "aws_s3_object" "layer_requests" {
   bucket = aws_s3_bucket.COBI_lambda_layers_2023.id
   key    = "layer_requests.zip" #path to the zip file
-  source = "./lambda_layers/layer_requests.zip" #path to the zip file
+  source = "./lambdas/lambda_layers/layer_requests.zip" #path to the zip file
 }
 
-# resource "aws_s3_bucket_object" "layer_xarray" {
+# resource "aws_s3_object" "layer_xarray" {
 #   bucket = aws_s3_bucket.COBI_lambda_layers_2023.id
 #   key    = "layer_xarray.zip" #path to the zip file
 #   source = "./lambda_layers/layer_xarray.zip" #path to the zip file
 # }
 
-# resource "aws_s3_bucket_object" "layer_motu" {
+# resource "aws_s3_object" "layer_motu" {
 #   bucket = aws_s3_bucket.COBI_lambda_layers_2023.id
 #   key    = "layer_motu.zip" #path to the zip file
 #   source = "./lambda_layers/layer_motu.zip" #path to the zip file
 # }
 
-# resource "aws_s3_bucket_object" "layer_numpy" {
+# resource "aws_s3_object" "layer_numpy" {
 #   bucket = aws_s3_bucket.COBI_lambda_layers_2023.id
 #   key    = "layer_numpy.zip" #path to the zip file
 #   source = "./lambda_layers/layer_numpy/python.zip" #path to the zip file
