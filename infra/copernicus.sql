aad5ddbca66317fa185bba1d777e88395d35282a
Xteom <mateodelaroche@gmail.com>
2023-05-26 18:18:22 -0600

    deleted duplicated where

--- infra/copernicus.sql
+++ infra/copernicus.sql
@@ -13,7 +13,6 @@ and (lon between -113.89444399967512  and -113.26666699967019 )
 
 
 select time, analysed_sst from athena_clean_data_db.seasurfacetemperature
-where (lat between 26.14999999957532 and  26.809166322814903)  
 where (lat between 26.14999999957532 and  26.809166322814903)
 and (lon between -113.89444399967512  and -113.26666699967019 )
 
